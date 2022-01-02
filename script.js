22c7ddbadabef154ffe76d63393f7c6cc959ce93
RamosFonseca <beidseitig@gmail.com>
2022-01-02 05:16:19 -0300

    Requisito 3 e 4

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const rgbColor = document.querySelector('#rgb-color');
+const colors = document.querySelector('#colors');
+const ball1 = document.querySelector('#ball1');
+const ball2 = document.querySelector('#ball2');
+const ball3 = document.querySelector('#ball3');
+const ball4 = document.querySelector('#ball4');
+const ball5 = document.querySelector('#ball5');
+const ball6 = document.querySelector('#ball6');
 
 
 
@@ -10,3 +17,10 @@ function backgroundColor () {
 }
 
 rgbColor.innerHTML = backgroundColor();
+
+ball1.style.backgroundColor = "rgb" + backgroundColor();
+ball2.style.backgroundColor = "rgb" + backgroundColor();
+ball3.style.backgroundColor = "rgb" + backgroundColor();
+ball4.style.backgroundColor = "rgb" + backgroundColor();
+ball5.style.backgroundColor = "rgb" + backgroundColor();
+ball6.style.backgroundColor = "rgb" + backgroundColor();
